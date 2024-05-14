e2d68b6c81b284d4e1b4d8d7a4a80f3cd2fb6d5d
Liviavia <melodyviel25@gmail.com>
2024-05-14 21:42:49 +0700

    profile sementara

---
+++ app/Http/Controllers/ProfileController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Models\PantiSosial;
+use Illuminate\Http\Request;
+
+class ProfileController extends Controller
+{
+    public function index(Request $request){
+        $user = PantiSosial::find($request->id);
+        return view('profile_pansos/profile');
+    }
+
+    public function test(){
+        return view('profile_pansos.test');
+    }
+}
---
+++ resources/views/profile_pansos/profile.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Profile</title>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset('css/profile.css')}}">
+</head>
+<body>
+    <div class="content row g-0">
+        <div class="left col-sm-3 d-none d-md-block">
+            {{-- <img src="{{asset('Image/general/templateImage.jpg')}}" alt="Sample photo" class="img-fluid"> --}}
+            <div>Pengaturan</div>
+            <img src="{{asset('Image/general/dummy_profile_pict.png')}}" alt="">
+            <a href="#" class="btn btn-block" id="btn-login">Pilih Foto</a>
+            <a href="#" class="btn btn-block" id="btn-login">Ubah Kata Sandi</a>
+            <a href="#" class="btn btn-block" id="btn-login">Keluar Akun</a>
+
+        </div>
+
+        <div class="col-lg-9">
+            <div class="card-body p-md-5 ">
+                <!-- HEADER -->
+                <div class="d-flex header">
+                    <img class="logo-img" src="{{asset('Image/general/logo.png')}}" alt="logo">
+                    <h3 class="mb-3">GivingGoods</h3>
+                </div>
+
+                {{-- OPTIONS --}}
+                <div class="card justify-content-center">
+                    <div class="card-body">
+                        <h5 class="card-title">Atur ulang kata sandi</h5>
+                        <form action="#" class="text" method="POST">
+                            @csrf
+                            <label for="email" id="email" class="email-text fw-normal lh-1">
+                                Masukkan email yang terdaftar. Kami akan mengirim kode verifikasi untuk atur ulang kata sandi.
+                            </label>
+                            <input type="text" name="email" class="email-input @error('email') is-invalid @enderror">
+                            @error('email')
+                                <label class="error-msg invalid-feedback fw-normal lh-1">
+                                    {{$message}}
+                                </label>
+                            @enderror
+                            <button type="submit" class="btn px-4 me-md-2 fw-normal" id="btn-next">
+                                Lanjut
+                            </button>
+                        </form>
+                        <a href="#" class="btn btn-block" id="btn-login">Kembali ke Halaman Masuk</a>
+                    </div>
+                </div>
+
+                <!-- Sudah punya akun? -->
+                <div class="d-flex bottom-txt">
+                    <div id="copyright-txt">©️GivingGoods | 2024</div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
+</html>
