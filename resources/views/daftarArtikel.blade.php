0c816b9aba43cbb264fdfd55dd962e8a3590e012
Clarabella Lius <liusclarabella@gmail.com>
2024-06-03 23:03:49 +0700

    f/cardsArtikel
    completed:
    - cards artikel
    
    needs to be done:
    - edit css
    - add artikel

--- resources/views/daftarArtikel.blade.php
+++ resources/views/daftarArtikel.blade.php
@@ -7,7 +7,20 @@
 @endsection
 
 @section('content')
- <h1>Daftarartikel</h1>
+<div class="daftarForum-top">
+    <h6 id="daftarForumTitle">Daftar Forum</h6>
+</div>
+
+<a href="" class="text-decoration-none text-dark">
+    <div class="card w-60" id="forum-cards">
+        <div class="card-body">
+            <div class="card-top">
+                <h5 class="card-title" id="judulForum">judul artikel</h5>
+            </div>
+                <p class="card-text">deskripsi artikel</p>
+        </div>
+    </div>
+</a>
 @endsection
 
 
