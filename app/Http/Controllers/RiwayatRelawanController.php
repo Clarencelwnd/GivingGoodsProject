490d933451094714133a46c1315e985755c518da
Clarencelwnd <clarencelauwandi88@gmail.com>
2024-06-16 10:23:08 +0700

    merapikan code

--- app/Http/Controllers/RiwayatRelawanController.php
+++ app/Http/Controllers/RiwayatRelawanController.php
@@ -8,26 +8,6 @@ use App\Models\Models\RegistrasiRelawan;
 
 class RiwayatRelawanController extends Controller
 {
-    // public function index()
-    // {
-    //     // Ambil data registrasi donatur beserta informasi donatur
-    //     $registrasiRelawan = RegistrasiRelawan::with('donaturRelawan')->get();
-
-    //    // Format tanggal dan jam menjadi satu string dan tambahkan ke variabel baru
-    // foreach ($registrasiRelawan as $registrasi) {
-    //     $tanggalKegiatan = date('Y-m-d', strtotime($registrasi->TanggalKegiatanMulaiRelawan)) . ' - ' . date('Y-m-d', strtotime($registrasi->TanggalKegiatanSelesaiRelawan));
-    //     $waktuKegiatan = date('H:i', strtotime($registrasi->JamMulaiRelawan)) . ' - ' . date('H:i', strtotime($registrasi->JamSelesaiRelawan));
-    //     $registrasi->setAttribute('tanggalKegiatan', $tanggalKegiatan);
-    //     $registrasi->setAttribute('waktuKegiatan', $waktuKegiatan);
-    // }
-
-    //     // Hitung jumlah donatur dengan status "Konfirmasi Diterima"
-    //     $jumlahKonfirmasiDiterima = RegistrasiRelawan::where('StatusRegistrasiRelawan', 'Terima')->count();
-
-    //     // Kirim data ke view beserta jumlah donatur yang telah dikonfirmasi
-    //     return view('RiwayatRelawan', compact('registrasiRelawan', 'jumlahKonfirmasiDiterima'));
-    // }
-
 
     public function index(Request $request)
     {
