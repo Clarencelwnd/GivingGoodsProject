96dd8de9535ee8419eb03c0ebc5301a5de6b642e
Clarabella Lius <liusclarabella@gmail.com>
2024-06-20 23:45:37 +0700

    fix: change css for change password blade

--- resources/views/ProfileDonaturRelawan/change_password.blade.php
+++ resources/views/ProfileDonaturRelawan/change_password.blade.php
@@ -1,25 +1,19 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Profile</title>
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Plus+Jakarta+Sans:ital,wght@0,200..800;1,200..800&display=swap" rel="stylesheet">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+@extends('generalPageDonaturRelawan.templateDonaturRelawan')
+
+@section('title', 'Ubah Kata Sandi')
+
+@section('stylesheets')
+    @parent
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <link rel="stylesheet" href="{{asset('css/Profile/change_password.css')}}">
-</head>
-<body>
-    <div class="card container justify-content-center">
-        <div class="card-header">
-            <p class="position">Profil > Ubah Kata Sandi</p>
-            <h5 class="title">Ubah Kata Sandi</h5>
-        </div>
+@endsection
+
+@section('content')
+    <div class="container">
         <div class="card-body">
-            <div class="desc-change-pass1">Atur ulang kata sandi</div>
-            <div class="desc-change-pass2" >Buat kata sandi baru yang kuat untuk akunmu</div>
+            <div class="desc-change-pass1">Ubah Kata Sandi</div>
+            <div class="desc-change-pass2">Atur ulang kata sandi</div>
+            <div class="desc-change-pass3" >Buat kata sandi baru yang kuat untuk akunmu</div>
             <form action="{{route('change_password_logic.donatur_relawan', ['id'=>$id])}}" class= "form" role="form" autocomplete="off" method="post">
                 @csrf
                 <div class="form-group">
@@ -40,7 +34,7 @@
                         </div>
                     @enderror
                 </div>
-                <div class="d-flex justify-content-end" id="button-style">
+                <div class="d-flex justify-content-end mb-3" id="button-style">
                     <a data-bs-toggle="modal" data-bs-target="#exitChangePasswordModal" class="btn btn-cancel" id="exitChangePassword">Batal</a>
                     <button type="submit" class="btn" id="btn-save">Simpan Perubahan </button>
                 </div>
@@ -87,10 +81,9 @@
             </div>
         </div>
     </div>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
     @if(session('success'))
         <script> var url = "{{route('profile.donatur_relawan', ['id'=>$id])}}" </script>
         <script src="{{ asset('js/Profile/change_password.js') }}"></script>
     @endif
-</body>
-</html>
+@endsection
