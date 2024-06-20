695a5ab566b5cfad5270884ba52c9d3887e1a79b
Clarencelwnd <clarencelauwandi88@gmail.com>
2024-06-20 14:38:19 +0700

    page pansos pagination blm selesai

--- app/Http/Controllers/PagePanSosController.php
+++ app/Http/Controllers/PagePanSosController.php
@@ -13,18 +13,67 @@ class PagePanSosController extends Controller
 {
 
     public function show($IDPantiSosial, $IDDonaturRelawan)
-    {
-        $pantiSosial = PantiSosial::findOrFail($IDPantiSosial);
-        $donaturRelawan = DonaturAtauRelawan::findOrFail($IDDonaturRelawan);
+{
+    // Mendapatkan informasi panti sosial berdasarkan ID
+    $pantiSosial = PantiSosial::findOrFail($IDPantiSosial);
 
-        // Mengambil kegiatan relawan dengan IDPantiSosial yang sesuai
-        $kegiatanRelawan = KegiatanRelawan::where('IDPantiSosial', $IDPantiSosial)->get();
+    // Mendapatkan informasi donatur/relawan berdasarkan ID
+    $donaturRelawan = DonaturAtauRelawan::findOrFail($IDDonaturRelawan);
 
-        // Mengambil kegiatan donasi dengan IDPantiSosial yang sesuai
-        $kegiatanDonasi = KegiatanDonasi::where('IDPantiSosial', $IDPantiSosial)->get();
+    // Mengambil kegiatan relawan dengan ID Panti Sosial yang sesuai
+    $kegiatanRelawan = KegiatanRelawan::where('IDPantiSosial', $IDPantiSosial)->get();
 
-        return view('DetailPantiSosial.PagePanSos', compact('pantiSosial', 'donaturRelawan', 'kegiatanRelawan', 'kegiatanDonasi'));
+    // Mengambil kegiatan donasi dengan ID Panti Sosial yang sesuai
+    $kegiatanDonasi = KegiatanDonasi::where('IDPantiSosial', $IDPantiSosial)->get();
+
+    // Inisialisasi array kosong untuk $activities
+    $activities = [];
+
+    $jenisDonasiIcons = [
+        'alat_tulis' => 'Image/donasi/alat_tulis.png',
+        'buku' => 'Image/donasi/buku.png',
+        'keperluan_rumah' => 'Image/donasi/keperluan_rumah.png',
+        'mainan' => 'Image/donasi/mainan.png',
+        'makanan' => 'Image/donasi/makanan.png',
+        'obat' => 'Image/donasi/obat.png',
+        'pakaian' => 'Image/donasi/pakaian.png',
+        'keperluan_ibadah' => 'Image/donasi/perlengkapan_ibadah.png',
+        'sepatu' => 'Image/donasi/sepatu.png',
+        'keperluan_mandi' => 'Image/donasi/toiletries.png'
+    ];
+
+    // Looping untuk menambahkan kegiatan relawan ke dalam $activities
+    foreach ($kegiatanRelawan as $relawan) {
+        $activities[] = (object) [
+            'IDKegiatanRelawan' => $relawan->IDKegiatanRelawan,
+            'NamaKegiatanRelawan' => $relawan->NamaKegiatanRelawan,
+            'GambarKegiatan' => $relawan->GambarKegiatanRelawan, // Ubah disini
+            'JenisKegiatanRelawan' => $relawan->JenisKegiatanRelawan,
+            'pantiSosial' => $pantiSosial,
+        ];
     }
+
+    // Looping untuk menambahkan kegiatan donasi ke dalam $activities
+    foreach ($kegiatanDonasi as $donasi) {
+        $activities[] = (object) [
+            'IDKegiatanDonasi' => $donasi->IDKegiatanDonasi,
+            'NamaKegiatanDonasi' => $donasi->NamaKegiatanDonasi,
+            'GambarKegiatan' => $donasi->GambarKegiatanDonasi, // Ubah disini
+            'JenisDonasiDibutuhkan' => $donasi->JenisDonasiDibutuhkan,
+            'pantiSosial' => $pantiSosial,
+            'jenisDonasiIcons'=>$jenisDonasiIcons,
+        ];
+    }
+
+
+
+    // Variabel ID untuk keperluan customisasi di view
+    $id = $IDDonaturRelawan;
+
+    // Mengirimkan data ke view dengan compact
+    return view('DetailPantiSosial.PagePanSos', compact('pantiSosial', 'donaturRelawan', 'activities', 'id'));
+}
+
 
 
 }
--- resources/views/DetailPantiSosial/PagePanSos.blade.php
+++ resources/views/DetailPantiSosial/PagePanSos.blade.php
@@ -5,13 +5,14 @@
 @section('stylesheets')
     @parent
     <link href="{{ asset('css/DetailPantiSosial/PagePanSos.css') }}" rel="stylesheet">
+    <link rel="stylesheet" href="{{ asset('css/components/pagination.css') }}">
     <script src="{{ asset('js/DetailPantiSosial/PagePanSos.js') }}"></script>
 @endsection
 
 @section('content')
     <div class="container">
         <div class="info-container">
-            <img src="{{ asset('image/login_reset_password/' . $pantiSosial->LogoPantiSosial) }}" class="donation-icon" alt="Logo Panti Sosial">
+            <img src="{{ $pantiSosial->LogoPantiSosial }}" class="donation-icon" alt="Logo Panti Sosial">
             <div class="info-text-container">
                 <div class="title">{{ $pantiSosial->NamaPantiSosial }}</div>
                 <div class="text-info">No. Registrasi: {{ $pantiSosial->NomorRegistrasiPantiSosial }}</div>
@@ -56,5 +57,78 @@
             </div>
         </div>
     </div>
+
+    <div class="daftarKegiatanContents">
+        <h4 id="daftar-kegiatan-title">Daftar Kegiatan</h4>
+
+        {{-- CARDS --}}
+        <div class="row" id="activity-cards">
+            @foreach ($activities as $activity)
+            <div class="col-md-4 mb-5 activity-card"
+                data-jenis-kegiatan="{{ isset($activity->NamaKegiatanRelawan) ? 'relawan' : 'donasi' }}"
+                data-jenis-donasi="{{ isset($activity->JenisDonasiDibutuhkan) ? implode(' ', explode(',', $activity->JenisDonasiDibutuhkan)) : '' }}"
+                data-jenis-relawan="{{ isset($activity->JenisKegiatanRelawan) ? implode(' ', explode(',', $activity->JenisKegiatanRelawan)) : '' }}">
+                @if (isset($activity->NamaKegiatanDonasi))
+                    <a href="{{ route('detailKegiatanDonasi', ['idKegiatanDonasi' => $activity->IDKegiatanDonasi, 'idDonaturRelawan' => $id]) }}" style="text-decoration: none; color: inherit;">
+                @elseif (isset($activity->NamaKegiatanRelawan))
+                    <a href="{{ route('detailKegiatanRelawan', ['idKegiatanRelawan' => $activity->IDKegiatanRelawan, 'idDonaturRelawan' => $id]) }}" style="text-decoration: none; color: inherit;">
+                @endif
+                    <div class="card">
+                        @if (isset($activity->NamaKegiatanRelawan))
+                            <img src="{{ $activity->GambarKegiatan }}" class="card-img-top" style="height: 14rem" alt="...">
+                        @elseif (isset($activity->NamaKegiatanDonasi))
+                            <img src="{{ $activity->GambarKegiatan }}" class="card-img-top" style="height: 14rem" alt="...">
+                        @endif
+
+                        <div class="card-body card-kegiatan">
+                            <h5 class="card-title" id="card-namaKegiatan">{{ $activity->NamaKegiatanRelawan ?? $activity->NamaKegiatanDonasi }}</h5>
+                            <p class="card-text" id="card-jenisDonasi">
+                                @if (isset($activity->NamaKegiatanRelawan))
+                                    <p class="card-text">Jenis relawan:
+                                        @if ($activity->JenisKegiatanRelawan == "Bencana_Alam")
+                                            Bencana Alam
+                                        @elseif ($activity->JenisKegiatanRelawan == "Darurat_Bencana")
+                                            Darurat Bencana
+                                        @elseif ($activity->JenisKegiatanRelawan == "Seni_Budaya")
+                                            Seni Budaya
+                                        @else
+                                            {{ $activity->JenisKegiatanRelawan }}
+                                        @endif
+                                    </p>
+                                @elseif (isset($activity->NamaKegiatanDonasi))
+                                    @php
+                                        $donasiTypes = explode(',', $activity->JenisDonasiDibutuhkan);
+                                        $donasiTypes = array_slice($donasiTypes, 0, 5);
+                                    @endphp
+                                    Jenis Donasi:
+                                    @foreach ($donasiTypes as $type)
+                                        @if(array_key_exists($type, $activity->jenisDonasiIcons))
+                                            <img id="jenisDonasiIcons" src="{{ asset($activity->jenisDonasiIcons[$type]) }}" alt="{{ $type }}">
+                                        @else
+                                            {{ $type }}
+                                        @endif
+                                    @endforeach
+                                @endif
+                            </p>
+                            <div class="d-flex justify-content-between">
+                                <p class="card-text" id="card-namaPanti">{{ $activity->pantiSosial->NamaPantiSosial }}</p>
+                                <p class="card-text" id="card-jenisDonasi">Jarak</p>
+                            </div>
+                        </div>
+                    </div>
+                </a>
+            </div>
+        @endforeach
+
+        </div>
+    </div>
+
     @endsection
-</html>
+
+
+{{-- @section('pagination')
+    <div class="pagination-container">
+
+        {{ $activities->appends(request()->input())->links('components.pagination') }}
+    </div>
+@endsection --}}
