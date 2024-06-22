b1cbc37fd5cf7e8e5c5f7c098e70e04e05b7a3a8
Clarabella Lius <liusclarabella@gmail.com>
2024-06-22 15:53:32 +0700

    fix: fix complete css kegiatan relawan

--- resources/views/GeneralPagePantiSosial/templatePage.blade.php
+++ resources/views/GeneralPagePantiSosial/templatePage.blade.php
@@ -21,11 +21,8 @@
     <div class="container">
         @include('components.sideBar', ['id' => $id])
 
-        @include('components.header', ['id'=> $id])
-        {{-- @section('header') --}}
         <div id="main">
             @include('components.header', ['id'=> $id])
-            {{-- @include('components.header', ['id' => $id]) --}}
             @yield('content')
             @yield('pagination')
             @include('components.footer')
